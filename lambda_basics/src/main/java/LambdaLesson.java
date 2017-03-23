16e7e34ea0f3f884b92088b3b6af5d61672a7465
SAKAI Yoshihito <y-sakai@esm.co.jp>
2017-03-23 16:34:57 +0900

    インデント修正

--- lambda_basics/src/main/java/LambdaLesson.java
+++ lambda_basics/src/main/java/LambdaLesson.java
@@ -2,18 +2,18 @@ import java.util.Comparator;
 import java.util.function.Function;
 
 public class LambdaLesson {
-    public static void lesson() {
+  public static void lesson() {
 
-        // 引数（Integer）を２倍にして返却するFunction
-        Function<> function = ...;
-        System.out.println(function.apply(3));
+    // 引数（Integer）を２倍にして返却するFunction
+    Function<> function = ...;
+    System.out.println(function.apply(3));
 
-        // 引数のStringを比較するComparator
-        Comparator<String> comparator = ...;
-        System.out.println(comparator.compare("aaa", "bbb"));
-    }
+    // 引数のStringを比較するComparator
+    Comparator<String> comparator = ...;
+    System.out.println(comparator.compare("aaa", "bbb"));
+  }
 
-    public static void main(String[] args) {
-        lesson();
-    }
+  public static void main(String[] args) {
+    lesson();
+  }
 }

972b30235a50ff863e7d06440db197b69a560ae0
SAKAI Yoshihito <y-sakai@esm.co.jp>
2017-03-23 15:59:33 +0900

    レッスン追加

---
+++ lambda_basics/src/main/java/LambdaLesson.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+import java.util.function.Function;
+
+public class LambdaLesson {
+    public static void lesson() {
+
+        // 引数（Integer）を２倍にして返却するFunction
+        Function<> function = ...;
+        System.out.println(function.apply(3));
+
+        // 引数のStringを比較するComparator
+        Comparator<String> comparator = ...;
+        System.out.println(comparator.compare("aaa", "bbb"));
+    }
+
+    public static void main(String[] args) {
+        lesson();
+    }
+}

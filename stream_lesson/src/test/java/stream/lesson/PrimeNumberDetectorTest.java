49d2b58109101c3783f3dcebf3ab33ecd9f79a81
kunou <t-kunou@esm.co.jp>
2017-08-18 09:28:49 +0900

    問題を追加

--- stream_lesson/src/test/java/stream/lesson/PrimeNumberDetectorTest.java
+++ stream_lesson/src/test/java/stream/lesson/PrimeNumberDetectorTest.java
@@ -16,11 +16,34 @@ public class PrimeNumberDetectorTest {
 
         return null;
     }
+
+    // int値を受け取り、並列Streamを使って0からその値までの全ての素数をListにして返すメソッドを実装してください
+    // ※ まだ答えを書いていません…
+    public List<Integer> detectParallel(int i) {
+        // TODO
+
+        return null;
+    }
   }
 
   @Test
   public void 素数判定() {
     List<Integer> primeNumbers = new PrimeNumberDetector().detect(20);
+
+    assertThat(primeNumbers.size(), is(8));
+    assertThat(primeNumbers.get(0), is(2));
+    assertThat(primeNumbers.get(1), is(3));
+    assertThat(primeNumbers.get(2), is(5));
+    assertThat(primeNumbers.get(3), is(7));
+    assertThat(primeNumbers.get(4), is(11));
+    assertThat(primeNumbers.get(5), is(13));
+    assertThat(primeNumbers.get(6), is(17));
+    assertThat(primeNumbers.get(7), is(19));
+  }
+
+  @Test
+  public void 素数判定並列版() {
+    List<Integer> primeNumbers = new PrimeNumberDetector().detectParallel(20);
 
     assertThat(primeNumbers.size(), is(8));
     assertThat(primeNumbers.get(0), is(2));

e410888ad313d6f5004fa45e83c69821cdfe8f17
kunou <t-kunou@esm.co.jp>
2017-08-18 09:20:33 +0900

    Streamを使うlessonを追加

---
+++ stream_lesson/src/test/java/stream/lesson/PrimeNumberDetectorTest.java
@@ -0,0 +1,35 @@
+package stream.lesson;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class PrimeNumberDetectorTest {
+
+  private class PrimeNumberDetector {
+    // int値を受け取り、Streamを使って0からその値までの全ての素数をListにして返すメソッドを実装してください
+    // ※ まだ答えを書いていません…
+    public List<Integer> detect(int i) {
+        // TODO
+
+        return null;
+    }
+  }
+
+  @Test
+  public void 素数判定() {
+    List<Integer> primeNumbers = new PrimeNumberDetector().detect(20);
+
+    assertThat(primeNumbers.size(), is(8));
+    assertThat(primeNumbers.get(0), is(2));
+    assertThat(primeNumbers.get(1), is(3));
+    assertThat(primeNumbers.get(2), is(5));
+    assertThat(primeNumbers.get(3), is(7));
+    assertThat(primeNumbers.get(4), is(11));
+    assertThat(primeNumbers.get(5), is(13));
+    assertThat(primeNumbers.get(6), is(17));
+    assertThat(primeNumbers.get(7), is(19));
+  }
+}

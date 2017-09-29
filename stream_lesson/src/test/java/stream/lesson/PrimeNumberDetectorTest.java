4a8e57b5c8a28504d02e82e2e1e5f0b9e9dd1960
kunou <t-kunou@esm.co.jp>
2017-09-29 09:53:49 +0900

    Add answer

--- stream_lesson/src/test/java/stream/lesson/PrimeNumberDetectorTest.java
+++ stream_lesson/src/test/java/stream/lesson/PrimeNumberDetectorTest.java
@@ -2,28 +2,56 @@ package stream.lesson;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
-import java.util.Arrays;
+
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 
 public class PrimeNumberDetectorTest {
 
   private class PrimeNumberDetector {
     // int値を受け取り、Streamを使って0からその値までの全ての素数をListにして返すメソッドを実装してください
-    // ※ まだ答えを書いていません…
     public List<Integer> detect(int i) {
-      // TODO
-      
-      return null;
+      return IntStream.range(2, i)
+        .filter(n -> isPrime(n))
+        .boxed()
+        .collect(Collectors.toList());
     }
 
     // int値を受け取り、並列Streamを使って0からその値までの全ての素数をListにして返すメソッドを実装してください
-    // ※ まだ答えを書いていません…
     public List<Integer> detectParallel(int i) {
-      // TODO
-
-      return null;
+      return IntStream.range(2, i)
+        .parallel()
+        .filter(n -> isPrime(n))
+        .boxed()
+        .sorted()
+        .collect(Collectors.toList());
     }
+
+    public boolean isPrime(int n) {
+      if (n < 2) {
+        return false;
+      } else if (n == 2) {
+        return true;
+      } else if ((n & 1) == 0) {
+        return false;
+      }
+
+      return IntStream.range(3, n).allMatch(x -> n % x != 0);
+    }
+  }
+
+  @Test
+  public void isPrime() {
+    assertThat(new PrimeNumberDetector().isPrime(14), is(false));
+    assertThat(new PrimeNumberDetector().isPrime(15), is(false));
+    assertThat(new PrimeNumberDetector().isPrime(16), is(false));
+    assertThat(new PrimeNumberDetector().isPrime(17), is(true));
+    assertThat(new PrimeNumberDetector().isPrime(18), is(false));
+    assertThat(new PrimeNumberDetector().isPrime(19), is(true));
+    assertThat(new PrimeNumberDetector().isPrime(20), is(false));
   }
 
   @Test

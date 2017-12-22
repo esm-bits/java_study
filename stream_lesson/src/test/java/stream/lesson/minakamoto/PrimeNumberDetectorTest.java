f8a660ff4b59a52bc5f1eee79c6a595542b5fb07
kunou <t-kunou@esm.co.jp>
2017-12-22 09:53:40 +0900

    ここまでのマージ漏れ

---
+++ stream_lesson/src/test/java/stream/lesson/minakamoto/PrimeNumberDetectorTest.java
@@ -0,0 +1,77 @@
+package stream.lesson.minakamoto;
+
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.IntStream.range;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import org.junit.Test;
+
+public class PrimeNumberDetectorTest {
+
+  private class PrimeNumberDetector {
+    // int値を受け取り、Streamを使って0からその値までの全ての素数をListにして返すメソッドを実装してください
+    // ※ まだ答えを書いていません…
+    public List<Integer> detect(int i) {
+      return range(0, i).filter(this::isPrime).boxed().collect(toList());
+    }
+
+    // int値を受け取り、並列Streamを使って0からその値までの全ての素数をListにして返すメソッドを実装してください
+    // ※ まだ答えを書いていません…
+    public List<Integer> detectParallel(int i) {
+      return range(0, i).parallel().filter(this::isPrime).boxed().collect(toList());
+    }
+
+    private boolean isPrime(int n) {
+      if (n < 2) {
+        return false;
+      }
+
+      for(int i=2;i<n;i++) {
+        if(n%i==0)
+          return false;
+      }
+      return true;
+    }
+
+  }
+
+  @Test
+  public void 素数判定() {
+    List<Integer> primeNumbers = new PrimeNumberDetector().detect(20);
+
+    assertThat(primeNumbers.size(), is(8));
+    assertThat(primeNumbers.get(0), is(2));
+    assertThat(primeNumbers.get(1), is(3));
+    assertThat(primeNumbers.get(2), is(5));
+    assertThat(primeNumbers.get(3), is(7));
+    assertThat(primeNumbers.get(4), is(11));
+    assertThat(primeNumbers.get(5), is(13));
+    assertThat(primeNumbers.get(6), is(17));
+    assertThat(primeNumbers.get(7), is(19));
+  }
+
+  @Test
+  public void 素数判定並列版() {
+    List<Integer> primeNumbers = new PrimeNumberDetector().detectParallel(20);
+
+    assertThat(primeNumbers.size(), is(8));
+    assertThat(primeNumbers.get(0), is(2));
+    assertThat(primeNumbers.get(1), is(3));
+    assertThat(primeNumbers.get(2), is(5));
+    assertThat(primeNumbers.get(3), is(7));
+    assertThat(primeNumbers.get(4), is(11));
+    assertThat(primeNumbers.get(5), is(13));
+    assertThat(primeNumbers.get(6), is(17));
+    assertThat(primeNumbers.get(7), is(19));
+  }
+}
